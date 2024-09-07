2c74304cd50035aecc1940ec3d05f4e40f6f4769
MaxMadeleine <Max.Madeleine.G@gmail.com>
2024-09-07 22:50:00 +0200

    header + dummy products made

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,3 +1,27 @@
+//#region SW WORKER
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("sw.js");
 }
+//#endregion
+//#region ADD TO CART
+document.addEventListener('DOMContentLoaded', () => {
+    let cartCount = 0;
+
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            addToCart();
+        });
+    });
+
+    function addToCart() {
+      cartCount++;
+      updateCartCount();
+    }
+
+    function updateCartCount() {
+      document.querySelector('.cartSpan').textContent = cartCount;
+    }
+  });
+  //#endregion
+  
